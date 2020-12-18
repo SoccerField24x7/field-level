21f3c9e477b5cf3246f2393123583e1fe26220ac
Jesse Quijano <jesse@quijano.net>
2020-12-18 09:24:13 -0800

    test dependency injection of redis into controller

--- FieldLevel/Controllers/PostsController.cs
+++ FieldLevel/Controllers/PostsController.cs
@@ -4,18 +4,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ServiceStack.Redis;
+using System.IO;
+using FieldLevel.Models;
 
 namespace FieldLevel.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class PostsController : ControllerBase
+    public class PostsController : Controller
     {
         private readonly ILogger<PostsController> _logger;
+        private readonly IRedisClient _redis;
 
-        public PostsController(ILogger<PostsController> logger)
+        public PostsController(ILogger<PostsController> logger, IRedisClient redis)
         {
             _logger = logger;
+            _redis = redis;
+        }
+
+        [HttpGet]
+        public JsonResult Main()
+        {
+            var redisPosts = _redis.As<Post>();
+
+            var testPost = new Post
+            {
+                Id = redisPosts.GetNextSequence(),
+                UserId = 10,
+                Title = "From Here to There",
+                Body = "Over the river and through the woods..."
+            };
+
+            try
+            {
+                _redis.Store(testPost);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return Json(testPost);
         }
     }
 }
