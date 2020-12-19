b08abc3021aa35aeb1dc0505bb2c7d8a9356c649
Jesse Quijano <jesse@quijano.net>
2020-12-18 16:20:27 -0800

    quick sample test to ensure base class and redis are working.

--- FieldLevel.Tests/DatabaseTests.cs
+++ FieldLevel.Tests/DatabaseTests.cs
@@ -1,15 +1,52 @@
 using System;
 using Xunit;
 using ServiceStack.Redis;
+using FieldLevel.Models;
+using System.Threading.Tasks;
 
 namespace FieldLevel.Tests
 {
-    public class DatabaseTests
+    public class DatabaseTests : IClassFixture<GlobalTestSetup>
     {
-        [Fact]
-        public void CanConnectRedis()
+        GlobalTestSetup _fixture;
+        RedisManagerPool _redispool;
+        public DatabaseTests(GlobalTestSetup fixture)
         {
-            Assert.True(true);
+            this._fixture = fixture;
+            this._redispool = new RedisManagerPool(fixture._connectionString);
         }
+
+        [Fact]
+        public async Task CanConnectRedis()
+        {
+            bool valid;
+
+            await using var redis = await _redispool.GetClientAsync();
+
+            var redisGuids = redis.As<Post>();
+
+            var testGuid = new Post
+            {
+                Id = 1,
+                UserId = 1,
+                Title = "From Here to There",
+                Body = "Over the river and through the woods to Grandmother's house we go."
+            };
+
+            try
+            {
+                await redisGuids.StoreAsync(testGuid, TimeSpan.FromSeconds(60));
+                valid = true;
+            }
+            catch (Exception)
+            {
+                valid = false;
+            }
+            
+
+            Assert.True(valid);
+        }
+
+
     }
 }
