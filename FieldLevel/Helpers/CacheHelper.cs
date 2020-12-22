23dc5fbe81ddc1d6aa83add533ed326f393f51fd
Jesse Quijano <jesse@quijano.net>
2020-12-22 09:36:08 -0800

    WIP: cache helper to store/expire cached data

---
+++ FieldLevel/Helpers/CacheHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FieldLevel.Models;
+using ServiceStack.Redis;
+
+namespace FieldLevel.Helpers
+{
+    public class CacheHelper
+    {
+        private IRedisClientsManagerAsync _manager;
+
+        public CacheHelper(IRedisClientsManagerAsync redisManager = null)
+        {
+            this._manager = redisManager;
+        }
+
+        public async Task CachePostList(List<Post> posts, TimeSpan? span = null)
+        {
+            await using var redis = await _manager.GetClientAsync();
+            var redisPosts = redis.As<Post>();
+
+            try
+            {
+                await redisPosts.StoreAllAsync(posts);
+                if (span != null)
+                {
+                    foreach (var post in posts)
+                    {
+                        await redisPosts.ExpireInAsync(post.Id, (TimeSpan)span);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public async Task<PostsDataCache> CacheLastPostsByUser(PostsDataCache cacheObject, TimeSpan? span = null)
+        {
+            await using var redis = await _manager.GetClientAsync();
+
+            var cacheType = redis.As<PostsDataCache>();
+
+             var result = await cacheType.StoreAsync(cacheObject);
+
+            if (span != null)
+                await cacheType.ExpireInAsync(cacheObject.Id, (TimeSpan)span);
+
+            return result;
+        }
+
+        public async Task<IRedisClientAsync> GetRedisClient()
+        {
+            return await _manager.GetClientAsync() ?? null;
+        }
+    }
+}
