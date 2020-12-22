3057972e8637db9c782ec80f3230b548b4e36210
Jesse Quijano <jesse@quijano.net>
2020-12-22 09:37:56 -0800

    tests to ensure we can fetch and parse data from api

--- FieldLevel.Tests/ExternalDataTests.cs
+++ FieldLevel.Tests/ExternalDataTests.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using Xunit;
+using FieldLevel.Helpers;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace FieldLevel.Tests
 {
     public class ExternalDataTests
     {
         [Fact]
-        public void CanConnect()
+        public async Task CanGetPostData()
         {
-            Assert.True(true);
+            var client = new ApiHelper();
+
+            var posts = await client.GetPostDataAsync();
+
+            Assert.Equal(100, posts.Count);
+        }
+
+        [Fact]
+        public async Task PostResultsHaveCorrectData()
+        {
+            var client = new ApiHelper();
+
+            var posts = await client.GetPostDataAsync();
+
+            var uniquesByUser = posts.GroupBy(s => s.UserId)
+                                    .Select(s => s.OrderByDescending(x => x.Id)
+                                    .FirstOrDefault()                                    )
+                                    .OrderBy(x => x.UserId)
+                                    .ToList(); // SO 470440
+
+            Assert.Equal(10, uniquesByUser[0].Id);
+            Assert.Equal(100, uniquesByUser[9].Id);
         }
     }
 }
