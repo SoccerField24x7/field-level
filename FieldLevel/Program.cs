44950c113ca8658236d38892fdb1b361c537bab7
Jesse Quijano <jesse@quijano.net>
2020-12-23 15:58:03 -0800

    add logging provider for DI

--- FieldLevel/Program.cs
+++ FieldLevel/Program.cs
@@ -18,6 +18,11 @@ namespace FieldLevel
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole(); // TODO: add real output target
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
