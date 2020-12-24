28b21ab2dc3ecb52a76fa954f206946d405fdb8f
Jesse Quijano <jesse@quijano.net>
2020-12-24 10:05:13 -0800

    add global setup, rename api service

--- FieldLevel.Tests/ExternalDataTests.cs
+++ FieldLevel.Tests/ExternalDataTests.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using Xunit;
-using FieldLevel.Helpers;
+using FieldLevel.Services;
 using System.Threading.Tasks;
 
 namespace FieldLevel.Tests
 {
-    public class ExternalDataTests
+    public class ExternalDataTests : IClassFixture<GlobalTestSetup>
     {
+        private GlobalTestSetup _fixture;
+        private ApiService _apiService;
+
+        public ExternalDataTests(GlobalTestSetup fixture)
+        {
+            this._fixture = fixture;
+            this._apiService = new ApiService();
+        }
+
         [Fact]
         public async Task CanGetPostData()
         {
-            var helper = new ApiHelper();
-
-            var posts = await helper.GetPostDataAsync();
+            var posts = await _apiService.GetPostDataAsync();
 
             Assert.Equal(100, posts.Count);
         }
@@ -20,11 +27,9 @@ namespace FieldLevel.Tests
         [Fact]
         public async Task PostResultsHaveCorrectData()
         {
-            var helper = new ApiHelper();
+            var posts = await _apiService.GetPostDataAsync();
 
-            var posts = await helper.GetPostDataAsync();
-
-            var uniquesByUser = helper.GetLastPostForEachUser(posts);
+            var uniquesByUser = _apiService.GetLastPostForEachUser(posts);
 
             Assert.Equal(10, uniquesByUser[0].Id);
             Assert.Equal(100, uniquesByUser[9].Id);
